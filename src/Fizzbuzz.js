428a3bf5a6be7c28b8fab5ea6af4a88a7ee674c0
Joe Newman <makerslaptop23@Makerss-MacBook-Pro-4.local>
2015-03-03 13:27:01 +0000

    is divisiblebyThree test refactored

--- src/Fizzbuzz.js
+++ src/Fizzbuzz.js
@@ -1,11 +1,6 @@
 function FizzBuzz() {
   FizzBuzz.prototype.isDivisibleByThree = function(number) {
-    if(number % 3 === 0) {
-      return true;
-    }
-    else {
-      return false;
-    };
-  };
-};
+    return number % 3 === 0
+  }
+}
 
